b5fafb9b2c076fecc669b8ee626942f5898607ac
zyjzzzzzzzz <1373108208@qq.com>
2026-04-26 02:21:12 +0800

    Add files via upload

---
+++ global.js
@@ -0,0 +1,28 @@
+const navbarLinks = {
+  home: true,
+  publications: true,
+  research: true,
+  jobs: true,
+  contact: true,
+};
+
+const globalData = {
+  image: "../files/profile.jpg",
+  resume: "../files/resume.pdf",
+  enAddress: "Your address ",
+  faAddress: " آدرس شما ",
+  phone: "09120000000",
+  email: "example@email.com",
+  twitterTitle: "",
+  twitter: "",
+  whatsappTitle: "WhatsApp number",
+  whatsapp: "https://api.whatsapp.com/send?phone=your_number",
+  googleScholarTitle: "Google Scholar title",
+  googleScholar: "@googleScholar",
+  linkedinTitle: "Linkedin Username",
+  linkedin: "https://www.linkedin.com/in/your_username/",
+  githubTitle: "Github Username",
+  github: "https://github.com/your_username",
+  orcidTitle: "Orcid Username",
+  orcid: "https://orcid.com/your_username",
+};
